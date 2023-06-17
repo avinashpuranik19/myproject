24ebfdbb47bf53dbea3837fc7c56dcb27e14aee7
avinashpuranik19 <111avinash@gmail.com>
2023-06-17 23:24:40 +0530

    first commit

---
+++ src/test/java/testRunner/dockertest.java
@@ -0,0 +1,29 @@
+package testRunner;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class dockertest {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		
+		DesiredCapabilities dc = DesiredCapabilities.chrome();
+		
+		URL url = new URL("http://localhost:4444/wd/hub");
+		
+		RemoteWebDriver driver = new RemoteWebDriver(url,dc);
+		
+		driver.get("http://www.google.co.in");
+		
+		System.out.println(driver.getTitle());
+		
+		driver.quit();
+		
+
+	}
+
+}
