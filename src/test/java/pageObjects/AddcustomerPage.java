263bd2171cae2fb45070f8302682ef5bd6864770
avinashpuranik19 <111avinash@gmail.com>
2023-07-11 15:32:16 +0530

    add customer

--- src/test/java/pageObjects/AddcustomerPage.java
+++ src/test/java/pageObjects/AddcustomerPage.java
@@ -1,5 +1,5 @@
 package pageObjects;
-//Code by Avinash on feature
+//Code by Avinash on feature - Retest again
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
